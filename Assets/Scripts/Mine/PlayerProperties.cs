3f55563143a1269ac44c9681a6584a245aa2e191
Brian <brobertjernigan@gmail.com>
2024-03-23 18:08:22 -0400

    Final Commit

--- Assets/Scripts/Mine/PlayerProperties.cs
+++ Assets/Scripts/Mine/PlayerProperties.cs
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Collision Resolution
+//Name: Brian Jernigan
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 03/25/2024
+/////////////////////////////////////////////
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- Assets/Scripts/Mine/Raycaster.cs
+++ Assets/Scripts/Mine/Raycaster.cs
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Collision Resolution
+//Name: Brian Jernigan
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 03/25/2024
+/////////////////////////////////////////////
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
